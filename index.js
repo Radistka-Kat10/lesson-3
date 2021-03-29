d7f373e00e74d9c212f48367702ee634dbb3483f
Korenkova Kateryna <korenkova.kateryna@gmail.com>
2021-03-29 19:18:23 +0300

    First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var UserAge = +prompt ('Сколько Вам лет?' , 'Сколько вам лет?');
+
+if (UserAge === 0) {
+    message ("лет");
+    
+} else if (UserAge === 1) {
+    message ("год");
+    
+} else if (UserAge < 4) {
+    message ("года");
+    
+
+} else {
+    message ("лет");
+
+}
+
+alert (UserAge + " " + message);
